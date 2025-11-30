9f4a228d6a6a1b06bbb3e6aa759bb3e54df95c57
chethanH N <chethanchethu26243@gmail.com>
2025-11-30 20:31:16 +0530

    Fix port binding for Render (use port 10000)

--- app.py
+++ app.py
@@ -23,5 +23,5 @@ def dashboard():
     return "Dashboard - basic version working! <a href='/login'>Back to Login</a>"
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))  # Render default is 10000
     app.run(host='0.0.0.0', port=port)
